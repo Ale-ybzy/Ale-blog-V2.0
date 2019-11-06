d0351496410df3d65c81c2dfb1bd7e82f5ac1714
Ale <410078368@qq.com>
2019-11-06 10:35:18 +0800

    sidebar

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,9 @@ from . import views
 #url模型
 app_name = 'blog'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts/<int:pk>/', views.detail, name='detail'),
+    path('', views.index, name='index'), #首页
+    path('posts/<int:pk>/', views.detail, name='detail'), #详情页
+    path('archives/<int:year>/<int:month>/', views.archive, name='archive'), #归档页
+    path('categories/<int:pk>/', views.category, name='category'), #分类页
+    path('tags/<int:pk>/', views.tag, name='tag'),  #标签页
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Category, Tag
 import markdown
 from markdown.extensions.toc import TocExtension
 import re
@@ -39,3 +39,22 @@ def detail(request, pk):
     post.toc = m.group(1) if m is not None else ''
 
     return render(request, 'blog/detail.html', context={'post': post})
+
+#归档页面视图
+def archive(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,  #Python 中调用属性的方式通常是 created_time.year，但是由于这里作为方法的参数列表，所以 django 要求我们把点替换成了两个下划线，即 created_time__year
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+#分类页面视图
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list':post_list})
+
+#标签页面视图
+def tag(request, pk):
+    t = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tag=t).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
