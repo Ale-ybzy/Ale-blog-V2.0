c7025e06c4fb4664035ce5453948138d88f0aad5
Ale <410078368@qq.com>
2019-11-13 22:37:49 +0800

    fabric

--- blogproject/settings/common.py
+++ blogproject/settings/common.py
@@ -20,13 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(isg8)^y8$3w-tw38_=j9njx+!z6^7b6k$l4*hh)s^o22d0%e-'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.ybzb.online'] #域名前加点表示允许该域名下的子域名访问
-
+# SECRET_KEY = '(isg8)^y8$3w-tw38_=j9njx+!z6^7b6k$l4*hh)s^o22d0%e-'
+#
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+#
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.ybzb.online'] #域名前加点表示允许该域名下的子域名访问
+#
 
 # Application definition
 
--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -1,8 +1,8 @@
 from .common import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '(isg8)^y8$3w-tw38_=j9njx+!z6^7b6k$l4*hh)s^o22d0%e-'
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = '(isg8)^y8$3w-tw38_=j9njx+!z6^7b6k$l4*hh)s^o22d0%e-'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
