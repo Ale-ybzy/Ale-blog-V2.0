1e63d7f1eb3f22ada7c574f5d014fc7756ef3ee2
Ale <410078368@qq.com>
2020-05-24 20:03:10 +0800

    run_time_stat

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse, request
+from django.http import HttpResponse
+from django.http import request
 from .models import Post, Category, Tag
 import markdown
 from markdown.extensions.toc import TocExtension
@@ -9,6 +10,8 @@ from django.utils.text import slugify
 from django.views.generic import ListView
 from django.contrib import messages
 from accstat.visit_info import change_info
+# 引入分页模块
+from django.core.paginator import Paginator
 
 # Create your views here.
 '''
@@ -23,30 +26,36 @@ def index(request):
     })
 '''
 
+
 # 首页视图函数
+# def index(request):
+#     change_info(request, '/')
+#     # model = Post
+#     # paginate_by = 8
+#
+#     post_list = Post.objects.all().order_by('-created_time')
+#     # 每页显示 6 篇文章
+#     paginate_by = Paginator(post_list, 6)
+#     # 获取 url 中的页码
+#     post_list = paginate_by.get_page(paginate_by)
+#
+#     # post_list = Post.objects.filter(paginate_by=8).order_by('-created_time')
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
 
-def index(request):
+
+class IndexView(ListView):
     model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+    # 指定 paginate_by 属性后开启分页功能，其值代表每一页包含多少篇文章
     paginate_by = 8
-    change_info(request, '/')
-    post_list = Post.objects.all().order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
-
-
-
-# class IndexView(ListView):
-#     model = Post
-#     template_name = 'blog/index.html'
-#     context_object_name = 'post_list'
-#     # 指定 paginate_by 属性后开启分页功能，其值代表每一页包含多少篇文章
-#     paginate_by = 8
 
 
 # 详情页视图函数
 def detail(request, pk):
     # get_object_or_404 方法，其作用就是当传入的 pk 对应的 Post 在数据库存在时，就返回对应的 post，如果不存在，就给用户返回一个 404 错误，表明用户请求的文章不存在。
     post = get_object_or_404(Post, pk=pk)
-
+    change_info(request, '/')
     # 阅读量+1
     post.increase_views()
 
@@ -73,25 +82,25 @@ def archive(request, year, month):
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
-class ArchivesView(ListView):
-    model = Post
-    template_name = 'blog/index.html'
-    context_object_name = 'post_list'
-
-    def get_queryset(self):
-        year = self.kwargs.get('year')
-        month = self.kwargs.get('month')
-        return super(ArchivesView, self).get_queryset().filter(created_time__year=year,
-                                                               created_time__month=month
-                                                               )
+# class ArchivesView(ListView):
+#     model = Post
+#     template_name = 'blog/index.html'
+#     context_object_name = 'post_list'
+#
+#     def get_queryset(self):
+#         year = self.kwargs.get('year')
+#         month = self.kwargs.get('month')
+#         return super(ArchivesView, self).get_queryset().filter(created_time__year=year,
+#                                                                created_time__month=month
+#                                                                )
 
 
 # 分类页面视图
 
-def category(request, pk):
-    cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+# def category(request, pk):
+#     cate = get_object_or_404(Category, pk=pk)
+#     post_list = Post.objects.filter(category=cate).order_by('-created_time')
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
 class CategoryView(ListView):
@@ -126,9 +135,11 @@ def search(request):
 
 # 自我简介页面
 def about(request):
+    change_info(request, '/')
     return render(request, 'blog/about.html')
 
 
 # 友情链接界面
 def blogroll(request):
+    change_info(request, '/')
     return render(request, 'blog/blogroll.html')
