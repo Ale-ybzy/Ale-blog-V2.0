7e299d03020f51883195694e5c68b260c7db5f5f
Ale_Local <410078368@qq.com>
2021-11-02 16:39:42 +0800

    docker_pro

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -7,21 +7,21 @@ SECRET_KEY = '(isg8)^y8$3w-tw38_=j9njx+!z6^7b6k$l4*hh)s^o22d0%e-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-#ALLOWED_HOSTS = ['*']  #域名前加点表示允许该域名下的子域名访问
-ALLOWED_HOSTS = ['.aleybzb.xyz', '127.0.0.1']  #域名前加点表示允许该域名下的子域名访问
+ALLOWED_HOSTS = ['*']  #域名前加点表示允许该域名下的子域名访问
+#ALLOWED_HOSTS = ['.aleybzb.xyz', '127.0.0.1']  #域名前加点表示允许该域名下的子域名访问
 
 #数据库配置
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/home/Ale/etc/my.cnf',
-        },
-        # 'HOST': 'blog_mysql',  # 容器名称
-        # 'NAME': 'ale_blog',  # 数据库名称
-        # 'USER': 'root',
-        # 'PASSWORD': 'Wl410078368/*',
-        # 'PORT': '13306',
-        # 'OPTIONS': {'charset': 'utf8mb4'},
+        # 'OPTIONS': {
+        #     'read_default_file': '/home/Ale/etc/my.cnf',
+        # },
+        'HOST': 'blog_mysql_pro',  # 容器名称
+        'NAME': 'ale_blog',  # 数据库名称
+        'USER': 'root',
+        'PASSWORD': 'Wl410078368/*',
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
--- compose/production/django/start.sh
+++ compose/production/django/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+cd /my_blog
 python manage_production.py migrate
 python manage_production.py collectstatic --noinput
-gunicorn blogproject.wsgi:application -w 4 -k gthread -b 0.0.0.0:8000 --chdir=/my_blog
+sleep 15
+gunicorn blogproject.wsgi:application -w 2 -k gthread -b 0.0.0.0:8000
