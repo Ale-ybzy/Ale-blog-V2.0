d6fde9a3cc8f720289344e0a9285f48097ed8906
Ale_Local <410078368@qq.com>
2021-11-03 10:27:34 +0800

    docker_pro

--- compose/production/mysql/init_st.sql
+++ compose/production/mysql/init_st.sql
@@ -5,3 +5,12 @@ set password for root@localhost = password('Wl410078368/*');
 grant all on *.* to root@'%' identified by 'Wl410078368/*' with grant option;
 -- use privileges
 flush privileges;
+
+
+-- 建库
+create database `ale_blog`;
+SET character_set_client = utf8;
+USE ale_blog;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
