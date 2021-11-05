b2826dc08df9d56e216ca4242ce53d974f9cd7ca
Ale_Local <410078368@qq.com>
2021-11-05 15:37:44 +0800

    docker_pro

--- compose/production/django/start.sh
+++ compose/production/django/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /my_blog
-sleep 100
+sleep 60
 python manage_production.py migrate
 python manage_production.py collectstatic --noinput
 gunicorn blogproject.wsgi:application -w 2 -k gthread -b 0.0.0.0:8000
